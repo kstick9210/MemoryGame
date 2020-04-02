61447c489462af98df79939d6ac1562fc111f0ce
Kathleen Stickel <kstick@Kathleens-MacBook-Pro.local>
2020-04-02 19:58:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,31 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 	else {alert("Sorry, try again.")}
+	
 }
+
+function flipCard(cardId) {
+	/*let cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+
+	let cardTwo = cards[1];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);*/
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+}
+
+flipCard(0);
+flipCard(2);
